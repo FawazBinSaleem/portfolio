95cf8c353306f2690d80a3b69ad9713c69c0a09c
FawazBinSaleem <fawaz.orton66@gmail.com>
2025-08-31 21:05:55 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      <h1>Fawaz's Personal Website</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        
+      </div>
+      <p className="read-the-docs">
+        Spider-Man is on the loose!!
+      </p>
+    </>
+  )
+}
+
+export default App
